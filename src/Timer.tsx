88b6761908d4dfba65694143382c58845d432526
j1nxie <rylieeeeexd@gmail.com>
2023-04-03 03:22:40 +0700

    feat: rename App to Timer + implement Timer

---
+++ src/Timer.tsx
@@ -0,0 +1,141 @@
+import { DateTime, Interval } from "luxon";
+import { useEffect, useState } from "react";
+import type { Duration } from "luxon";
+import "./Timer.css";
+
+type MissionDictionary = Record<string, Array<string>>;
+
+const missions: MissionDictionary = {
+	Monday: ["Tactical Drill", "Resource Search", "Tough Siege", "Solid Defense", "Fierce Attack"],
+	Tuesday: [
+		"Tactical Drill",
+		"Cargo Escort",
+		"Aerial Threat",
+		"Fierce Attack",
+		"Fearless Protection",
+	],
+	Wednesday: [
+		"Tactical Drill",
+		"Resource Search",
+		"Aerial Threat",
+		"Unstoppable Charge",
+		"Fearless Protection",
+	],
+	Thursday: [
+		"Tactical Drill",
+		"Cargo Escort",
+		"Tough Siege",
+		"Solid Defense",
+		"Unstoppable Charge",
+	],
+	Friday: [
+		"Tactical Drill",
+		"Resource Search",
+		"Aerial Threat",
+		"Solid Defense",
+		"Fierce Attack",
+	],
+	Saturday: [
+		"Tactical Drill",
+		"Cargo Escort",
+		"Resource Search",
+		"Tough Siege",
+		"Fierce Attack",
+		"Unstoppable Charge",
+		"Fearless Protection",
+	],
+	Sunday: [
+		"Tactical Drill",
+		"Cargo Escort",
+		"Tough Siege",
+		"Aerial Threat",
+		"Unstoppable Charge",
+		"Fearless Protection",
+	],
+};
+
+function getTime(): [DateTime, DateTime, Duration] {
+	const localDt = DateTime.now();
+	const serverDt = DateTime.utc().setZone("UTC-7");
+	let resetTime = DateTime.fromFormat("04:00", "HH:mm", { zone: "UTC-7" });
+
+	if (resetTime < serverDt) {
+		resetTime = resetTime.plus({ day: 1 });
+	}
+
+	let diff = resetTime.diff(serverDt, ["hours", "minutes", "seconds"]);
+
+	if (resetTime < serverDt) {
+		diff = diff.plus({ hours: 24 });
+	}
+
+	return [localDt, serverDt, diff];
+}
+
+function Timer() {
+	let [localDt, serverDt, diff] = getTime();
+	const [localDate, setLocalDate] = useState(localDt.toFormat("DDD TTT"));
+	const [serverDate, setServerDate] = useState(serverDt.toFormat("DDD TTT"));
+	// TODO: render singular hour and minutes
+	const [resetPeriod, setResetPeriod] = useState(`${diff.hours} hours, ${diff.minutes} minutes`);
+	const [weekday, setWeekday] = useState(DateTime.utc().setZone("UTC-7").weekdayLong);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			[localDt, serverDt, diff] = getTime();
+			setLocalDate(localDt.toFormat("DDD TTT"));
+			setServerDate(serverDt.toFormat("DDD TTT"));
+			setResetPeriod(`${diff.hours} hours, ${diff.minutes} minutes`);
+
+			const weekday = serverDt.weekdayLong;
+
+			setWeekday(weekday);
+		});
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+
+	return (
+		<div className="Timer">
+			<div className="container">
+				<div className="card">
+					<div className="time-left">
+						<section>
+							<h3>Server Reset Time</h3>
+							<span>04:00 UTC-7</span>
+						</section>
+						<section>
+							<h3>Time Until Reset</h3>
+							<span>{resetPeriod}</span>
+						</section>
+					</div>
+					<div className="time-right">
+						<section>
+							<h3>Local Time</h3>
+							<span>{localDate}</span>
+						</section>
+						<section>
+							<h3>Server Time</h3>
+							<span>{serverDate}</span>
+						</section>
+					</div>
+				</div>
+				<div className="card weekday">
+					<div className="time">
+						<section>
+							<h3>Current Weekday</h3>
+							<span>{weekday}</span>
+						</section>
+					</div>
+				</div>
+				{missions[weekday].map((mission) => (
+					<div className="time">{mission}</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default Timer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import App from "./App";
 import Header from "./Header";
+import Timer from "./Timer";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
 		<Header />
+		<Timer />
 	</React.StrictMode>
 );
